bea79dcb9e7adafd3d614d588d66530d0a17db56
vifas <victorfaustino12@gmail.com>
2022-03-03 18:37:42 -0300

    node

---
+++ estudos/parametros/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Oia");
+});
+
+app.get("/sobre",function(req,res){
+    res.send("Pagina sobre");
+})
+
+app.get("/blog", function(req,res){
+    res.send("Bem-vindo ao meu blog!");
+});
+
+app.get('/ola/:nome/:cargo', function(req,res){
+    res.send("<h1>Oia "+req.params.nome+"</h1>"+"<h2> Seu cargo e: "+req.params.cargo+"</h2>");
+    
+})
+
+app.listen(8081, function(){
+    console.log("Servidor rodando");
+});
